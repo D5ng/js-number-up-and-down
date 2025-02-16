bb09ae494f51a6bcc379c91d31aa825732c66e1f
d5ng <dong5ffice@gmail.com>
2025-02-16 11:07:21 +0900

    ♻️ Refactor: 불필요한 코드 제거 및 프린터 출력 함수 세분화

--- src/index.constants.js
+++ src/index.constants.js
@@ -1,11 +1,4 @@
 /**
- * 게임 상태
- */
-export const MAX_NUMBER = 50
-export const MIN_NUMBER = 1
-export const LIMIT_COUNT = 5
-
-/**
  * 프린터 색상 강조
  */
 const PRINT_COLOR = {
@@ -19,20 +12,28 @@ const PRINT_COLOR = {
 /**
  * 프린터 출력 함수
  */
+
 export const PRINT = Object.freeze({
-  init: `컴퓨터가 1~50 사이의 숫자를 선택했습니다. 숫자를 맞춰보세요.`,
-  userInputError: PRINT_COLOR.red("숫자는 1~50 사이로 입력해주세요."),
-  input: PRINT_COLOR.sky("숫자 입력: "),
-  restart: PRINT_COLOR.sky("게임을 다시 시작하시겠습니까? (yes/no):"),
-  validateResult: {
-    down: PRINT_COLOR.purple("다운"),
-    up: PRINT_COLOR.purple("업"),
-    answer: PRINT_COLOR.purple("정답!"),
+  settings: {
+    minAndMax: `[게임 설정] 게임 시작을 위해 최소 값, 최대 값을 입력해주세요. (예: 1, 50)`,
+    availableAttempts: `[게임 설정] 게임 시작을 위해 진행 가능 횟수를 입력해주세요.`,
   },
-  end: PRINT_COLOR.sky("게임을 종료합니다."),
-  answer: (playCount) => PRINT_COLOR.green(`축하합니다! ${playCount}번 만에 숫자를 맞추셨습니다.`),
-  prevGuess: (prevInputList) => PRINT_COLOR.yellow(`이전 추측: ${prevInputList.join(", ")}`),
-  excced: (answer) => PRINT_COLOR.red(`5회 초과! 숫자를 맞추지 못했습니다. (정답: ${answer})`),
+  play: {
+    restart: PRINT_COLOR.sky("게임을 다시 시작하시겠습니까? (yes/no):"),
+    validateResult: {
+      down: PRINT_COLOR.purple("다운"),
+      up: PRINT_COLOR.purple("업"),
+      answer: PRINT_COLOR.purple("정답!"),
+    },
+    end: PRINT_COLOR.sky("게임을 종료합니다."),
+    start: (min, max) => `[게임 시작] ${min}~${max} 사이의 숫자를 선택했습니다. 숫자를 맞춰보세요.`,
+    userInputError: (min, max) => PRINT_COLOR.red(`숫자는 ${min}~${max} 사이로 입력해주세요.`),
+    answer: (playCount) => PRINT_COLOR.green(`축하합니다! ${playCount}번 만에 숫자를 맞추셨습니다.`),
+    prevGuess: (prevInputList) => PRINT_COLOR.yellow(`이전 추측: ${prevInputList.join(", ")}`),
+    excced: (answer, limitCount) =>
+      PRINT_COLOR.red(`${limitCount}회 초과! 숫자를 맞추지 못했습니다. (정답: ${answer})`),
+  },
+  input: PRINT_COLOR.sky("숫자 입력: "),
 })
 
 export const invalidNumberMessage = "반드시 숫자를 입력해주세요!"
