7d222f880a6b24e17fa0359d554f2e17c9c88a86
d5ng <dong5ffice@gmail.com>
2025-02-18 00:01:45 +0900

    ♻️ Refactor: 파일, 함수명 리팩토링

--- src/utils/getRandomNumber.js
+++
@@ -1,8 +0,0 @@
-/**
- * @param {number} min 최소값
- * @param {number} max 최대값
- * @returns
- */
-export function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min
-}
