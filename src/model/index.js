b375932a3f2f9d5df98810b698c8dcd5706d2dcf
d5ng <dong5ffice@gmail.com>
2025-02-23 17:07:39 +0900

    ♻️ Refactor: 즉시 실행 함수로 리팩토링

--- src/model/index.js
+++ src/model/index.js
@@ -1,14 +1,15 @@
 import { useState } from "../utils"
 
-const initialState = {
-  prevInputList: [],
-  count: 0,
-  answer: 0,
-  min: 0,
-  max: 0,
-  limitCount: 0,
-}
+export const { state, setState } = (() => {
+  const initialState = {
+    prevInputList: [],
+    count: 0,
+    answer: 0,
+    min: 0,
+    max: 0,
+    limitCount: 0,
+  }
 
-const [state, setState] = useState(initialState)
-
-export { state, setState }
+  const [state, setState] = useState(initialState)
+  return { state, setState }
+})()
